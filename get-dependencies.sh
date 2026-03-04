b11665d6838a5676e068e16e86cf0ec4314e2f56
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-03 21:07:08 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm xournalpp
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
