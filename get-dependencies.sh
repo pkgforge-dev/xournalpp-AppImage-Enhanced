e17e6fd19c9f7ed45a6561f7b28f9d70c211f525
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-03 21:50:00 -0400

    add `patchelf`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm xournalpp
+pacman -Syu --noconfirm xournalpp patchelf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
